c7b4d69af4edb1fc75fd44270d04e9d96e89e169
jubin369 <jubinayoob369@gmail.com>
2022-03-04 12:58:43 +0530

    Adds code

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+module.exports = {
+    env: {
+        MONGO_URI: "mongodb+srv://jubin:Check123@cluster0.hh3t3.mongodb.net/Apollo?retryWrites=true&w=majority"
+    }
+}
---
+++ pages/api/graphql.js
@@ -0,0 +1,24 @@
+import  {  ApolloServer  }  from  "apollo-server-micro";
+import typeDefs from  "./schemas";
+import resolvers from  "./resolvers";
+import dbConnect from '../../utils/dbConnect';
+
+dbConnect();
+
+const  apolloServer  =  new  ApolloServer({  typeDefs,  resolvers  });
+
+const startServer = apolloServer.start();
+
+export default async function handler(req, res) {
+
+  await startServer;
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
